e4d6078e5f7cdeebdf53fa2d69006a94399be893
Bréwen CHENEAU <brewen.cheneau@gmail.com>
2025-04-30 23:46:21 +0200

    modif pwd

--- config/bdd.php
+++ config/bdd.php
@@ -2,6 +2,6 @@
 
 define('DB_URL', "localhost");
 define('DB_USER', "userGsb");
-define('DB_PWD', "userGsb$926");
+define('DB_PWD', "secret");
 define('DB_NAME', "gsb_frais");
 define('DB_DSN', "mysql:host=" . DB_URL . ";dbname=" . DB_NAME . ";charset=UTF8");
